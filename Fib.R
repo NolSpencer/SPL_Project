70ecc961eeec649524c52c570f803657d565d75f
NolSpencer <77764696+NolSpencer@users.noreply.github.com>
2021-12-12 17:17:14 -0500

    Update Fib.R

--- Fib.R
+++ Fib.R
@@ -1,7 +1,7 @@
 recurse_fibonacci <- function(n) {
-    if(n <= 1) {
-        return(n)
-    } else {
+if(n <= 1) {
+return(n)
+}else {
 return(recurse_fibonacci(n-1) + recurse_fibonacci(n-2))
 }
 }
